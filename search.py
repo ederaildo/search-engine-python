432a7f4992167ee3d73218e273c3d6f2e146dbb9
Ederaildo Fontes <ederaildo@gmail.com>
2017-11-06 22:10:55 -0200

    Versão inicial com a busca funcionando

--- search.py
+++ search.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+#-*- coding:utf-8 -*-
+
 
 #***************************************************
 # Search Engine simples em Python
@@ -7,23 +10,54 @@
 import os
 import string
 from os import listdir
-from os.path import basename, isdir, isfile
+from os.path import basename
 import sys
 
 DATA_DIR = "data"
 
-def search(termo):
-	files = listdir(DATA_DIR)
+def buscaTermo(conteudo, termo):
+	conteudo = conteudo.lower()
+	termoSplit = termo.split( )
 
-	#for f in files:
-		#print f
-	print termo[1]
+	encontrou = True
+
+	for t in termoSplit:
+		if conteudo.find(t.lower()) == -1:
+			encontrou = False
+	
+	if encontrou:
+		return True
+
+	return False
+
+
+def busca(termo):
+	arquivos = listdir(DATA_DIR)
+	arquivos.sort()
+	count = 0
+	arquivosEncontrados = []
+
+	for arq in arquivos:
+		conteudo = open(DATA_DIR + "/" + arq, "r").read()
+
+		if buscaTermo(conteudo, termo):
+			#print file
+			count +=1
+			arquivosEncontrados.append(arq)
+
+
+	print 'Foram encontradas {0} ocorrências pelo termo "{1}" '.format(count, termo)
+	print 'Os arquivos que possuem "{0}" são:'.format(termo)
+	
+	for arqF in arquivosEncontrados:
+		print "data/{0}".format(arqF)
+
 
 
 
 try:
-	search(sys.argv)
+	busca(sys.argv[1])
 except Exception as e:
-	print "Erro: ".format(e.errno,e.strerror)
+	print "Erro: ", e
 	raise
 
